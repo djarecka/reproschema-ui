01274308716c593e55c88f8fd32537bc7c55147a
Dorota Jarecka <djarecka@gmail.com>
2023-03-10 11:48:11 -0500

    testing

--- src/router/index.js
+++ src/router/index.js
@@ -30,15 +30,15 @@ export default new Router({
   ],
 });
 
-if (from.query.auth_token && !to.query.auth_token) {
-  if (from.path === to.path) {
-    next(false);
-  } else {
-    next({
-      path: to.path,
-      query: { ...to.query, auth_token: from.query.auth_token },
-    });
+
+router.beforeEach((to, from, next) => {
+  if (from.query.auth_token && !to.query.auth_token) {
+    if (to.path === from.path) {
+      // console.log('Identical routes detected')
+      return // This is a no-no via the documentation, but a bug in routing to identical routes strips query params, and this prevents that
+    }
+    next({path: to.path, query: {auth_token: from.query.auth_token}})
   }
-} else {
-  next();
-}
+
+  next()
+})
